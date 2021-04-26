6862a73184eb7248bd0ebaef05949958cb3ed459
jessijoke <jessijoke@yahoo.com>
2021-04-26 01:12:59 -0400

    commit tree 0bc5edb3c2259c12dc8b91f6d82624a497dd74fb

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function writeCards(arr, desc) {
+    let thankYouMessage = [];
+    for (let i = 0; i < arr.length; i++) thankYouMessage.push(`Thank you, ${arr[i]}, for the wonderful ${desc} gift!`);
+    return thankYouMessage;
+}
+
+function countDown(num) {
+    let counter = num;
+    while (counter >= 0) {
+        console.log(counter);
+        counter-=1;
+    }
+}
